db348cca9e3f3cc786b457a08c5d21359f29c0f3
Roman Kudryashov <rskudryashov@gmail.com>
2020-02-07 14:51:25 +0300

    Apollo Server should pass auth header to downstream services

--- apollo-server/gateway.js
+++ apollo-server/gateway.js
@@ -1,15 +1,28 @@
 const {ApolloServer} = require("apollo-server");
-const {ApolloGateway} = require("@apollo/gateway");
+const {ApolloGateway, RemoteGraphQLDataSource} = require("@apollo/gateway");
+
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+    willSendRequest({request, context}) {
+        request.http.headers.set('Authorization', context.authHeaderValue);
+    }
+}
 
 const gateway = new ApolloGateway({
     serviceList: [
         {name: "auth-service", url: "http://localhost:8081/graphql"},
         {name: "planet-service", url: "http://localhost:8082/graphql"},
         {name: "satellite-service", url: "http://localhost:8083/graphql"}
-    ]
+    ],
+    buildService({name, url}) {
+        return new AuthenticatedDataSource({url});
+    },
 });
 
-const server = new ApolloServer({gateway, subscriptions: false});
+const server = new ApolloServer({
+    gateway, subscriptions: false, context: ({req}) => ({
+        authHeaderValue: req.headers.authorization
+    })
+});
 
 server.listen().then(({url}) => {
     console.log(`🚀 Server ready at ${url}`);
