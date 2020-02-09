115ca9a20126c99b2138e8b73adfd800468f9d33
Roman Kudryashov <rskudryashov@gmail.com>
2020-02-09 12:43:15 +0300

    Add execution input customizers

---
+++ satellite-service/src/main/kotlin/io/graphqlfederation/satelliteservice/web/graphql/HeaderValueProviderGraphQLExecutionInputCustomizer.kt
@@ -0,0 +1,26 @@
+package io.graphqlfederation.satelliteservice.web.graphql
+
+import graphql.ExecutionInput
+import io.gqljf.federation.tracing.HTTPRequestHeaders
+import io.micronaut.configuration.graphql.DefaultGraphQLExecutionInputCustomizer
+import io.micronaut.context.annotation.Primary
+import io.micronaut.core.async.publisher.Publishers
+import io.micronaut.http.HttpRequest
+import org.reactivestreams.Publisher
+import javax.inject.Singleton
+
+@Singleton
+// mark it as primary to override the default one
+@Primary
+class HeaderValueProviderGraphQLExecutionInputCustomizer : DefaultGraphQLExecutionInputCustomizer() {
+
+    override fun customize(executionInput: ExecutionInput, httpRequest: HttpRequest<*>): Publisher<ExecutionInput> {
+        val context = HTTPRequestHeaders { headerName ->
+            httpRequest.headers[headerName]
+        }
+
+        return Publishers.just(executionInput.transform {
+            it.context(context)
+        })
+    }
+}
